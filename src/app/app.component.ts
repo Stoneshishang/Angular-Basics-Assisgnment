42ba9b6be204b3b8417bf37bb8a1ffde74be55be
Shi <stoneshishang@gmail.com>
2021-12-20 15:45:55 -0500

    getting the index when using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent {
   serverName = '';
 
   showSecret = false;
-  log: number[] = [];
+  log: any[] = [];
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
